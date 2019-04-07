553c8160bf87efdf4f37e025c12d21ccc9ce0d91
Alexander Schmidt <28296890+arxel-sc@users.noreply.github>
2019-04-07 14:17:10 +0200

    not completely done, more like saving

--- bar-wallpaper-changer.py
+++ bar-wallpaper-changer.py
@@ -4,40 +4,17 @@
 # imports                                                                      #
 ################################################################################
 
-import requests
-import logging
-from bs4 import BeautifulSoup
-import time
 import os
 import sys
+import time
+import json
+import logging
+import requests
+from bs4 import BeautifulSoup
+
 
 __version__ = '0.0.2'
 __author__ = 'arxel-sc'
-
-
-################################################################################
-# init folders                                                                 #
-################################################################################
-
-HOME_FOLDER = os.path.expanduser('~')
-SHARE_DIR = os.path.join(HOME_FOLDER, '.local', 'share', 'MARK_bar_wallpaper')
-LOG_DIR = os.path.join(SHARE_DIR, 'logs')
-PIC_DIR = os.path.join(SHARE_DIR, 'pics')
-LOG_FILE = os.path.join(LOG_DIR, 'MARK_bar_wallpaper.log')
-# set timer to 3 minutes
-rotation_time = 3 * 60
-
-try:
-    if not os.path.isdir(SHARE_DIR):
-        os.mkdir(SHARE_DIR)
-except Exception as err:
-    os.mkdirs(SHARE_DIR)
-if not os.path.isdir(LOG_DIR):
-    os.mkdir(LOG_DIR)
-if not os.path.isdir(PIC_DIR):
-    os.mkdir(PIC_DIR)
-
-# TODO: Implement log rotation
 
 
 ################################################################################
@@ -51,64 +28,81 @@ logging.debug(LOG_FILE)
 logging.info(f'Starting version {__version__}')
 
 
-################################################################################
-# get infos from website                                                       #
-################################################################################
-
-requests.packages.urllib3.disable_warnings()
-sitecontent = requests.get("https://www.marksalzburg.at") #, verify=False)
-soup = BeautifulSoup(sitecontent.text, "html.parser")
-events = soup.find_all("div", "event-content")
+def get_website_info():
+    requests.packages.urllib3.disable_warnings()
+    sitecontent = requests.get("https://www.marksalzburg.at", verify=False)
+    soup = BeautifulSoup(sitecontent.text, "html.parser")
+    events = soup.find_all("div", "event-content")
+    return events
 
 
-################################################################################
-# get images                                                                   #
-################################################################################
-
-old_pics = os.listdir(PIC_DIR)
-new_pics = []
-
-for event in events:
-    image_url = event.find('img')['src']
-    image_name = image_url.split('/')[-1]
-    new_pics.append(image_name)
-    if image_name not in old_pics:
-        with open(os.path.join(PIC_DIR, image_name), 'wb') as pic:
-            r = requests.get(image_url, allow_redirects=True, verify=False)
-            pic.write(r.content)
-            logging.info(f'Downloaded {image_name} to pics folder!')
+def download_images():
+    for event in events:
+        image_url = event.find('img')['src']
+        image_name = image_url.split('/')[-1]
+        new_pics.append(image_name)
+        if image_name not in old_pics:
+            with open(os.path.join(PIC_DIR, image_name), 'wb') as pic:
+                r = requests.get(image_url, allow_redirects=True, verify=False)
+                pic.write(r.content)
+                logging.info(f'Downloaded {image_name} to pics folder!')
 
 
-################################################################################
-# remove old pictures                                                          #
-################################################################################
+def remove_old_pics(old, new):
+    '''
+    Remove old pictures from the pic-directory to not clutter it.
+    '''
+    for element in old:
+        if element not in new:
+            os.remove(os.path.join(PIC_DIR, element))
+            logging.info(f'Removed {element} from pics folder!')
 
-for element in old_pics:
-    if element not in new_pics:
-        os.remove(os.path.join(PIC_DIR, element))
-        logging.info(f'Removed {element} from pics folder!')
+
+def set_wallpaper():
+    # os.system(f'feh --bg-scale {PIC_DIR}/{element}')
+    os.system(f"gsettings set org.gnome.desktop.background picture-uri file://{PIC_DIR}/{element}")
+    os.system("gesettings set org.gnome.desktop.background picture-options 'scaled'")
+    logging.info(f'Set new Wallpaper: {element}')
+    time.sleep(rotation_time)
 
 
-################################################################################
-# start main loop                                                              #
-################################################################################
+def main():
+    try:
+        while True:
+            number_of_pics = len(new_pics)
+            for n, element in enumerate(new_pics):
+                if n + 1 == number_of_pics:
+                    break
+                else:
+                    set_wallpaper()
+    except Exception as err:
+        logging.error(err)
+    finally:
+        logging.critical('Shutting down')
+        sys.exit()
 
-try:
-    while True:
-        number_of_pics = len(new_pics)
-        for n, element in enumerate(new_pics):
-            if n + 1 == number_of_pics:
-                break
-            else:
-                # os.system(f'feh --bg-scale {PIC_DIR}/{element}')
-                os.system(f"gsettings set org.gnome.desktop.background picture-uri file://{PIC_DIR}/{element}")
-                os.system("gesettings set org.gnome.desktop.background picture-options 'scaled'")
-                logging.debug(f'{n}, {element}')
-                logging.info(f'Set new Wallpaper: {element}')
-            time.sleep(rotation_time)
-except Exception as err:
-    logging.error(err)
-finally:
-    logging.critical('Shutting down')
-    sys.exit()
+
+if __name__ == '__main__':
+
+    HOME_FOLDER = os.path.expanduser('~')
+    SHARE_DIR = os.path.join(HOME_FOLDER, '.local', 'share', 'bar-wallpaper-changer')
+    LOG_DIR = os.path.join(SHARE_DIR, 'logs')
+    PIC_DIR = os.path.join(SHARE_DIR, 'pics')
+    LOG_FILE = os.path.join(LOG_DIR, 'bar-wallpaper-changer.log')
+
+    try:
+        if not os.path.isdir(SHARE_DIR):
+            os.mkdir(SHARE_DIR)
+    except Exception as err:
+        os.mkdirs(SHARE_DIR)
+    if not os.path.isdir(LOG_DIR):
+        os.mkdir(LOG_DIR)
+    if not os.path.isdir(PIC_DIR):
+        os.mkdir(PIC_DIR)
+
+    old_pics = os.listdir(PIC_DIR)
+    new_pics = []
+
+    remove_old_pics(old_pics, new_pics)
+    main()
 
