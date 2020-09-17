f4609bc80877bdcc6644fa7c7ce193314e58fe76
Alexander Schmidt <28296890+arxel-sc@users.noreply.github>
2020-09-17 19:44:35 +0200

    decided to approach th eproblem differently

--- bar-wallpaper-changer-cli.py
+++ bar-wallpaper-changer-cli.py
@@ -1,25 +1,26 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 import os
 import logging
 import bwc_methods.core
 
 
-__version__ = '0.1.0'
-__author__ = 'arxel-sc'
+__version__ = "0.1.0"
+__author__ = "arxel-sc"
 
 
-LOG_DIR = os.path.join(os.getcwd(), 'logs')
-PIC_DIR = os.path.join(os.getcwd(), 'pics')
-LOG_FILE = os.path.join(LOG_DIR, 'bar-wallpaper-changer.log')
+LOG_DIR = os.path.join(os.getcwd(), "logs")
+PIC_DIR = os.path.join(os.getcwd(), "pics")
+LOG_FILE = os.path.join(LOG_DIR, "bar-wallpaper-changer.log")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-    formatting = '%(asctime)s: %(levelname)s: in %(module)s: %(message)s'
+    formatting = "%(asctime)s: %(levelname)s: in %(module)s: %(message)s"
     logging.basicConfig(filename=LOG_FILE, level=logging.DEBUG,
                         format=formatting)
     logging.debug(LOG_FILE)
-    logging.info(f'Starting version {__version__}')
+    logging.info(f"Starting version {__version__}")
 
     bwc_methods.core.main(PIC_DIR)
+
